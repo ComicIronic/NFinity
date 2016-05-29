6f3450588b18032a66b1da6c659ec25845faf43d
ComicIronic <comicironic@gmail.com>
2016-05-30 00:51:52 +0100

    Added Preferences file for future work

---
+++ src/nfinity/nfinity/preferences/Preferences.java
@@ -0,0 +1,13 @@
+package nfinity.nfinity.preferences;
+
+/**
+ * Insert description here
+ *
+ * @author Comic
+ * @since 29/05/2016 2016
+ */
+public class Preferences {
+    public boolean EnforceNullable = false;
+
+    public boolean DisableAny = false;
+}
