48e397539bb067bf068e98997826b889d79a11c8
ComicIronic <comicironic@gmail.com>
2016-05-31 00:39:55 +0100

    Moved addInclude to NProject and updated functionality

--- src/nfinity/nfinity/nproject/NProject.java
+++ src/nfinity/nfinity/nproject/NProject.java
@@ -1,8 +1,12 @@
 package nfinity.nfinity.nproject;
 
+import nfinity.nfinity.Nterpreter;
 import nfinity.nfinity.nassembly.NAssembly;
+import nfinity.nfinity.ncontext.NContext;
+import nfinity.nfinity.preferences.Preferences;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Dictionary;
 import java.util.List;
 
@@ -20,4 +24,14 @@ public class NProject {
     public List<Path> Files;
 
     public NAssembly Assembly;
+
+    public NContext CurrentContext;
+
+    public Preferences Preferences = new Preferences();
+
+    public void addInclude(String pathstring) {
+        Path foundPath = Paths.get(Defines.get("FILE_DIR"), pathstring.substring(1, pathstring.length() - 2));
+
+        Files.add(Nterpreter.convertFile(foundPath));
+    }
 }
