0f31fcc94468de5b77029208b9f8dbf4f9904ed3
ComicIronic <comicironic@gmail.com>
2016-06-20 22:31:58 +0100

    Added main code as placeholder.

--- src/nfinity/nfinity/Main.java
+++ src/nfinity/nfinity/Main.java
@@ -1,8 +1,17 @@
 package nfinity.nfinity;
 
+
+import nfinity.nfinity.nproject.NProject;
+
+import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+	    NProject project = Nterpreter.openProject(Paths.get(args[0]));
+
+        Nterpreter.assemble(project);
     }
 }
